637c8220886c4b651b9f6d8d4af1a9d118fa95d1
Mady Ogorek <madyogorek@Madys-MacBook-Pro.local>
2025-09-14 13:31:49 -0500

    print out all latitudes and longitudes

--- hurricanes/management/commands/convert_html_to_csv.py
+++ hurricanes/management/commands/convert_html_to_csv.py
@@ -1,6 +1,7 @@
 # myapp/management/commands/convert_html_to_csv.py
+from bs4 import BeautifulSoup
 from django.core.management.base import BaseCommand
-import pandas as pd
+
 import os
 
 class Command(BaseCommand):
@@ -16,8 +17,36 @@ class Command(BaseCommand):
 
         # Read the first table from the HTML
         with open(html_path, "r", encoding="utf-8") as f:
-            html_content = f.read()
-        print(html_content)
+            lxml = BeautifulSoup(f, "lxml")
+        # Extract text inside <pre>
+        pre_text = lxml.find("pre").get_text()
+
+        # Split into lines
+        lines = pre_text.strip().splitlines()
+
+        hurricane_count = 0
+        date = ""
+        name = ""
+        readings = ""
+        i = 0
+        hit_florida_already = False
+        while i < len(lines):
+            #remove all spaces
+            clean_line = lines[i].replace(" ", "")
+            split_line = clean_line.split(",")
+            if len(split_line) == 4: #header line
+                date = split_line[0]
+                name = split_line[1]
+                readings = int(split_line[2])
+                hit_florida_already = False
+                i += 1
+                continue
+            hit_florida = False
+            for reading in range(readings):
+                latitude =  split_line[4]
+                longitude = split_line[5]
+                print(latitude, longitude)
+            i += 1
 
 
         self.stdout.write(self.style.SUCCESS(f"✅ Conversion complete! Saved as {output_path}"))
